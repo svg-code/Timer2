c226a499e3e5d3d185002e2c8e8427b04dfde526
support <support@DESKTOP-2BH1VSI>
2021-11-10 14:00:52 +0100

    Add project files.

---
+++ Timer2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace Timer2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                var myPath = @"c:\New Folder\TimeLog2.txt";
+                //File.Create(myPath);
+                Stack<DateTime> LogTime = new Stack<DateTime>();
+
+                for (int i = 0; i < 15; i++)
+                {
+                    LogTime.Push(DateTime.Now);
+                    Thread.Sleep(10000);
+                }
+
+                foreach (var item in LogTime)
+                {
+                    File.AppendAllLines(myPath, new String[] { item.ToString() });
+                }
+            }
+        }
+    }
+}
